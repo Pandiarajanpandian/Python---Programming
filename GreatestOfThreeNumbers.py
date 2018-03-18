3587c99737b12b42dbd30304a7dd3b4f2bb8bf18
Pandiarajanpandian <37370193+Pandiarajanpandian@users.noreply.github.com>
2018-03-18 11:50:10 +0530

    Update GreatestOfThreeNumbers.py

--- GreatestOfThreeNumbers.py
+++ GreatestOfThreeNumbers.py
@@ -1,9 +1,9 @@
 a=(input("enter the first number"))
 b=(input("enter the second number"))
-c=(input("rnter the third number"))
-if a>b and a>c:
-  print("a is greater")
-elif b>c and b>a:
-  print("b is greater")
+c=(input("enter the third number"))
+if a>=b and a>=c:
+  print(a)
+elif b>=c and b>=a:
+  print(b)
 else:
-  print('c is greater')
+  print(c)
