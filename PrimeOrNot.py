81ddb9e177792a54e564707c4f58204026035801
Pandiarajanpandian <37370193+Pandiarajanpandian@users.noreply.github.com>
2018-03-18 20:45:39 +0530

    Create PrimeOrNot.py

---
+++ PrimeOrNot.py
@@ -0,0 +1,9 @@
+num=(input("enter a number"))
+if num>1:
+  for i in range(2,num):
+    if (num%i)==0:
+      print("the number is not a prime")
+  else:
+      print("prime")
+else:
+    print("not a prime")
