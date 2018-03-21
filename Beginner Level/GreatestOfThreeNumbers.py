5afd52481b35807c2b7be34f921ca79ce5dce0c3
Pandiarajanpandian <37370193+Pandiarajanpandian@users.noreply.github.com>
2018-03-21 09:45:39 +0530

    Create GreatestOfThreeNumbers.py

---
+++ Beginner Level/GreatestOfThreeNumbers.py
@@ -0,0 +1,9 @@
+a=(input("enter the first number"))
+b=(input("enter the second number"))
+c=(input("enter the third number"))
+if a>=b and a>=c:
+  print(a)
+elif b>=c and b>=a:
+  print(b)
+else:
+  print(c)
