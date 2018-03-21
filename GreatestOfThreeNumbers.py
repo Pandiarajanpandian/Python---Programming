e35401a6d3de3e29385a90764256a41a8bf3c292
Pandiarajanpandian <37370193+Pandiarajanpandian@users.noreply.github.com>
2018-03-21 10:18:21 +0530

    Delete GreatestOfThreeNumbers.py

--- GreatestOfThreeNumbers.py
+++
@@ -1,9 +0,0 @@
-a=(input("enter the first number"))
-b=(input("enter the second number"))
-c=(input("enter the third number"))
-if a>=b and a>=c:
-  print(a)
-elif b>=c and b>=a:
-  print(b)
-else:
-  print(c)
